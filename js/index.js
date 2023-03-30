319421d0104c98497c43b0672179e44287593209
DavideBS <104096563+DavideBS@users.noreply.github.com>
2023-03-30 15:02:28 +0200

    fetch index.html - js - style.css

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-const images = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
 
 
 
@@ -57,22 +33,17 @@ const { createApp } = Vue
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     }
                 ],
-         hover: true,
-        
+         
+         autoplay: null,
          currentIndex: 0,
-         interval: setInterval(this.next, 1000),
+        
       }
     },
     computed: {
-        startSlide(){
-            console.log(this.hover)
-            return this.interval
-        },
-        mouseOverLeaveStart(){
-            if(!this.hover){
-                clearInterval(this.interval)
-            }
-        }
+      play(){
+        this.startSlider()
+      }
+        
 
     },
     methods: {
@@ -88,14 +59,22 @@ const { createApp } = Vue
             } else{
                 this.currentIndex--;
             }
+        },
+        sliderColumnChange(index){
+           this.currentIndex = index
+
+        },
+        startSlider(){
+          this.autoplay = setInterval(()=>{
+            this.next()
+          }, 2000)
+        },
+        stopSlider(){
+          clearInterval(this.autoplay);
+          this.autoplay = null;
+
         }
-        // ,
-        // mouseLeaveStart(){
-        //     setInterval(this.next, 1000)
-        // },
-        // mouseOverClear(){
-        //     clearInterval(this.interval)
-        // },
+        
         
     }
   }).mount('#app')
